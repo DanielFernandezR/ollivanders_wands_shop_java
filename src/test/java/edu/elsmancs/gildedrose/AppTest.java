3deef8701c2650fac3504f5d7beef8a77c577976
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-01-27 12:59:27 +0100

    Clase NormalItem con Bugs (seguir en clase)

--- src/test/java/edu/elsmancs/gildedrose/AppTest.java
+++ src/test/java/edu/elsmancs/gildedrose/AppTest.java
@@ -8,10 +8,22 @@ public class AppTest {
     @Test
     public void crearItem() {
 
-        Item item = new Item("+5 Dexterity Vest", 10, 20);
+    	Item item = new Item("+5 Dexterity Vest", 10, 20);
 
         assertEquals("+5 Dexterity Vest", item.getName());
         assertEquals("10", item.getSell_in().toString());
         assertEquals("20", item.getQuality().toString());
+    }
+    
+    @Test
+    public void normalItem_works() {
+
+    	NormalItem item = new NormalItem("+5 Dexterity Vest", 10, 20);
+        item.update_quality();
+        item.setQuality();
+        
+        assertEquals("+5 Dexterity Vest", item.getName());
+        assertEquals("9", item.getSell_in().toString());
+        assertEquals("19", item.getQuality().toString());
     }
 }
