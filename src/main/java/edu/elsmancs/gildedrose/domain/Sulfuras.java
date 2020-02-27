02ba7e2b7d76ebf1a741410075e8e0dedaefb6bb
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-27 13:34:05 +0100

    style(): Eliminar espacios en blanco

--- src/main/java/edu/elsmancs/gildedrose/domain/Sulfuras.java
+++ src/main/java/edu/elsmancs/gildedrose/domain/Sulfuras.java
@@ -1,12 +1,12 @@
 package edu.elsmancs.gildedrose.domain;
 
-public class Sulfuras extends NormalItem{
-	
+public class Sulfuras extends NormalItem {
 
 	public Sulfuras(String name, int sell_in, int quality) {
 		super(name, sell_in, quality);
-    }
-	
+	}
+
 	@Override
-	public void updateQuality() {}
+	public void updateQuality() {
+	}
 }
