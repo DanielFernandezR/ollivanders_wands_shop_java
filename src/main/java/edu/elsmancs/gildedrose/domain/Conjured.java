0ad3238060b85dc37997ebbc8d692eeafa7813ac
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-27 13:34:33 +0100

    feat

--- src/main/java/edu/elsmancs/gildedrose/domain/Conjured.java
+++ src/main/java/edu/elsmancs/gildedrose/domain/Conjured.java
@@ -1,20 +1,18 @@
 package edu.elsmancs.gildedrose.domain;
 
-public class Conjured extends NormalItem{
+public class Conjured extends NormalItem {
 
-	
 	public Conjured(String name, int sell_in, int quality) {
 		super(name, sell_in, quality);
-    }
-	
+	}
+
 	@Override
 	public void updateQuality() {
-    	if (getSell_in() > 0) {
-    		computeQuality(-2);
-    	}
-    	else {
-    		computeQuality(-4);
-    	}
-    	setSell_in();
-    }
+		if (getSell_in() > 0) {
+			computeQuality(-2);
+		} else {
+			computeQuality(-4);
+		}
+		setSell_in();
+	}
 }
